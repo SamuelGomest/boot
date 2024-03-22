54ee4d7428714e58f091e85513d32eda673afafc
SamuelGomest <130321613+SamuelGomest@users.noreply.github.com>
2024-03-22 01:26:15 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+var myCarousel = document.querySelector('#carouselExampleSlidesOnly');
+  var carousel = new bootstrap.Carousel(myCarousel, {
+    interval: 2000 // Troca de imagem a cada 2 segundos (opcional)
+    // Outras opções podem ser adicionadas conforme necessário
+  });
